d73eed3d2164c746f13fde7a8faa796e90757a4e
leeuz <agathayuji@gmail.com>
2024-10-17 22:44:58 -0700

    PID 2

--- src/main/java/frc/robot/commands/PIDTurnccw.java
+++ src/main/java/frc/robot/commands/PIDTurnccw.java
@@ -1,5 +1,50 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveTrain;
+
 public class PIDTurnccw {
+    DriveTrain dt;
+    double setPointAngle;
+    PIDController PID = new PIDController(0,9, 0, 0);
+
+    public PIDTurnccw(DriveTrain dt, double setPoint){
+
+        this.dt = dt;
+        this.setPointAngle = setPoint;
+
+        PID.setTolerance(5);
+        addRequirements(this.dt);
+    }
+
+    @Override
+    public void initialize() {
+        dt.reset();
+        dt.tankDrive(0, 0);
+    }
+
+    double output = PID.calculate(90.0);
+    @Override
+    public void execute() {
+        if (output > 0){
+            if (output > 0){
+                dt.tankDrive(output, -(output));
+            }
+            else {
+                dt.tankDrive(-(output), output);
+            }
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        dt.tankDrive(0,0);
     
+    }
+
+    @Override
+    public boolean isFinished() {
+        return PID.atSetPoint();
+    }
 }
